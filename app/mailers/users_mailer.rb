d924dabeb54eea07d7146b7aeda993e09445a6bc
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-06-29 23:23:31 +0530

    enabled logger in production

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -10,12 +10,10 @@ class UsersMailer < ActionMailer::Base
     #I18n.locale.to_s = @notification.user.locale.to_s
     #set_locale @notification.user.locale
     logger.debug "mailer2"
-    mail(:from => "#{I18n.t('site.name')} <#{I18n.t('site.email.contact')}>", :to => @notification.user.email, :subject => @notification.email_subject).deliver
+    mail(:from => "#{I18n.t('site.name')} <#{I18n.t('site.email.contact')}>", :to => @notification.user.email, :subject => @notification.email_subject)
     logger.debug "mailer3"
     #I18n.locale = old_locale
     I18n.locale = old_locale if I18n.locale.to_s != @notification.user.locale.to_s
     logger.debug "mailer4"
   end
 end
-
-
