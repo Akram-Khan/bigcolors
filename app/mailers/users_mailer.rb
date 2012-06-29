36c1c75f36a74bbb3a11e9d80ab4d1845c339ba4
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-06-29 21:09:15 +0530

    enabled logger in production

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -6,9 +6,9 @@ class UsersMailer < ActionMailer::Base
     @notification = notification
     old_locale = I18n.locale
     #I18n.locale = @notification.user.locale if I18n.locale.to_s != @notification.user.locale.to_s # we need this if to avoid stack overflow in controller
-    I18n.locale = :en
+    #I18n.locale.to_s = :en
     logger.debug "mailer2"
-    mail(:from => "#{I18n.t('site.name')} <#{I18n.t('site.email.contact')}>", :to => @notification.user.email, :subject => @notification.email_subject)
+    mail(:from => "#{'site.name'} <#{'site.email.contact'}>", :to => @notification.user.email, :subject => @notification.email_subject)
     logger.debug "mailer3"
     #I18n.locale = old_locale if I18n.locale.to_s != @notification.user.locale.to_s
     logger.debug "mailer4"
