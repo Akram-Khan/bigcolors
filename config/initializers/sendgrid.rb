309fcbac0ec774cf94dd260c687049b9bc18765c
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-06-29 19:31:09 +0530

    removed sendgrid, all mails to go to bigcolors@akaruilabs.com

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,15 +1,15 @@
 begin
-  if Rails.env.production?
+  #if Rails.env.production?
     ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'catarse',
-    :password       =>  Configuration[:sendgrid],
-    :domain         => 'heroku.com'
+    :user_name      => 'bigcolors@akaruilabs.com',
+    :password       =>  'ams1terdam',
+    :domain         => 'bigcolors.webbyapp.com'
     }
     ActionMailer::Base.delivery_method = :smtp
-  end
+  #end
 rescue
   nil
 end
