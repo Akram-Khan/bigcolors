fc808cb8988036a8ae6cf0043ac0d9546a86491d
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-07-12 08:06:09 +0530

    changed email

--- app/mailers/credits_mailer.rb
+++ app/mailers/credits_mailer.rb
@@ -1,10 +1,10 @@
 class CreditsMailer < ActionMailer::Base
   include ERB::Util
-  default :from => "bigcolors <info@bigcolors.com>"
+  default :from => "bigcolors <bigcolors@akaruilabs.com>"
 
   def request_refund_from(backer)
     @backer = backer
     @user = backer.user
-    mail(:to => 'info@bigcolors.com', :subject => I18n.t('credits_mailer.request_refund_from.subject', :name => @user.name))
+    mail(:to => 'bigcolors@akaruilabs.com', :subject => I18n.t('credits_mailer.request_refund_from.subject', :name => @user.name))
   end
 end
